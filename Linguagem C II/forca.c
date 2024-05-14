f302d23bbac5d25c90433137b5af82387faa2607
Fernando Petri <fernando.petri01@gmail.com>
2024-05-13 21:23:17 -0300

    Continuando curso de C II

---
+++ Linguagem C II/forca.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char secretPhrase[20];
+    char guesses[26];
+    int tries = 0;
+
+    sprintf(secretPhrase, "MELANCIA");
+
+    int guessed = 0;
+    int strangled = 0;
+
+    do
+    {
+
+        char guess;
+
+        printf("Chute uma letra:\n\n");
+        scanf(" %c", &guess);
+        printf("\n");
+
+        guesses[tries] = guess;
+        tries++;
+
+        for (int i = 0; i < strlen(secretPhrase); i++)
+        {
+            int wasGuessed = 0;
+
+            for (int j = 0; j < tries; j++)
+            {
+                if (guesses[j] == secretPhrase[i])
+                {
+                    wasGuessed = 1;
+                    break;
+                }
+            }
+
+            if (wasGuessed)
+            {
+                printf("%c ", secretPhrase[i]);
+            }
+            else
+            {
+                printf("_ ");
+            }
+        }
+
+        printf("\n\n");
+
+    } while (!guessed && !strangled);
+
+    return 0;
+}
